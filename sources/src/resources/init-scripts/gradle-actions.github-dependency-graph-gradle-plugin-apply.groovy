177cef6d022506e034ffe75a61a4c85dd08b9d07
daz <daz@gradle.com>
2024-01-28 10:09:10 -0700

    Provide an override for the dependency-graph plugin version

--- sources/src/resources/init-scripts/gradle-actions.github-dependency-graph-gradle-plugin-apply.groovy
+++ sources/src/resources/init-scripts/gradle-actions.github-dependency-graph-gradle-plugin-apply.groovy
@@ -4,12 +4,13 @@ buildscript {
       return System.getProperty(name) ?: System.getenv(envVarName)
   }
   def pluginRepositoryUrl = getInputParam('gradle.plugin-repository.url') ?: 'https://plugins.gradle.org/m2'
+  def dependencyGraphPluginVersion = getInputParam('dependency-graph-plugin.version') ?: '1.1.1'
 
   repositories {
     maven { url pluginRepositoryUrl }
   }
   dependencies {
-    classpath "org.gradle:github-dependency-graph-gradle-plugin:1.1.1"
+    classpath "org.gradle:github-dependency-graph-gradle-plugin:${dependencyGraphPluginVersion}"
   }
 }
 apply plugin: org.gradle.github.GitHubDependencyGraphPlugin
