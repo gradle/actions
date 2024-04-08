9f06e0c9f347967ee544ee59d6f5a202c9faae1a
daz <daz@gradle.com>
2024-04-07 18:30:45 -0600

    Bump to CCUD plugin v2.0 everywhere

--- sources/test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
+++ sources/test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
@@ -17,7 +17,7 @@ import java.util.zip.GZIPOutputStream
 
 class BaseInitScriptTest extends Specification {
     static final String DEVELOCITY_PLUGIN_VERSION = '3.17'
-    static final String CCUD_PLUGIN_VERSION = '1.13'
+    static final String CCUD_PLUGIN_VERSION = '2.0'
 
     static final TestGradleVersion GRADLE_3_X = new TestGradleVersion(GradleVersion.version('3.5.1'), 7, 9)
     static final TestGradleVersion GRADLE_4_X = new TestGradleVersion(GradleVersion.version('4.10.3'), 7, 10)
