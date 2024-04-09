e1aa1e4cf7918c85715cbd173727d138ad126a37
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-04-09 20:39:16 +0200

    Create toolchains.xml dynamically based on envs

---
+++ sources/src/caching/gradle-user-home-utils.ts
@@ -0,0 +1,43 @@
+import path from 'path'
+import fs from 'fs'
+
+export function readResourceFileAsString(...paths: string[]): string {
+    // Resolving relative to __dirname will allow node to find the resource at runtime
+    const absolutePath = path.resolve(__dirname, '..', '..', '..', 'sources', 'src', 'resources', ...paths)
+    return fs.readFileSync(absolutePath, 'utf8')
+}
+
+/**
+ * Iterate over all `JAVA_HOME_{version}_{arch}` envs and construct the toolchain.xml.
+ *
+ * @VisibleForTesting
+ */
+export function getPredefinedToolchains(): string | null {
+    const javaHomeEnvs: string[] = []
+    for (const javaHomeEnvsKey in process.env) {
+        if (javaHomeEnvsKey.startsWith('JAVA_HOME_')) {
+            javaHomeEnvs.push(javaHomeEnvsKey)
+        }
+    }
+    if (javaHomeEnvs.length === 0) {
+        return null
+    }
+    // language=XML
+    let toolchainsXml = `<?xml version="1.0" encoding="UTF-8"?>
+<toolchains>
+<!-- JDK Toolchains installed by default on GitHub-hosted runners -->
+`
+    for (const javaHomeEnv of javaHomeEnvs) {
+        const version = javaHomeEnv.match(/JAVA_HOME_(\d+)_/)?.[1]!
+        toolchainsXml += `  <toolchain>
+    <provides>
+      <version>${version}</version>
+    </provides>
+    <configuration>
+      <jdkHome>\${env.${javaHomeEnv}</jdkHome>
+    </configuration>
+  </toolchain>\n`
+    }
+    toolchainsXml += `</toolchains>\n`
+    return toolchainsXml
+}
---
+++ sources/test/jest/predefined-toolchains.test.ts
@@ -0,0 +1,60 @@
+import {getPredefinedToolchains} from "../../src/caching/gradle-user-home-utils";
+
+describe('predefined-toolchains', () => {
+    const OLD_ENV = process.env
+    afterAll(() => {
+        process.env = OLD_ENV
+    });
+
+    describe('returns', () => {
+        it('null if no JAVA_HOME_ envs are set', async () => {
+            jest.resetModules()
+            process.env = { ...OLD_ENV }
+
+            const predefinedToolchains = getPredefinedToolchains()
+            expect(predefinedToolchains).toBe(null)
+        })
+        it('valid toolchains.xml if JAVA_HOME_ envs are set', async () => {
+            jest.resetModules()
+            process.env = {
+                ...OLD_ENV,
+                "JAVA_HOME_8_X64": "/jdks/foo_8",
+                "JAVA_HOME_11_X64": "/jdks/foo_11",
+                "JAVA_HOME_21_ARM64": "/jdks/foo_21",
+            }
+
+            const predefinedToolchains = getPredefinedToolchains()
+            expect(predefinedToolchains).toBe(
+                // language=XML
+`<?xml version="1.0" encoding="UTF-8"?>
+<toolchains>
+<!-- JDK Toolchains installed by default on GitHub-hosted runners -->
+  <toolchain>
+    <provides>
+      <version>8</version>
+    </provides>
+    <configuration>
+      <jdkHome>\${env.JAVA_HOME_8_X64</jdkHome>
+    </configuration>
+  </toolchain>
+  <toolchain>
+    <provides>
+      <version>11</version>
+    </provides>
+    <configuration>
+      <jdkHome>\${env.JAVA_HOME_11_X64</jdkHome>
+    </configuration>
+  </toolchain>
+  <toolchain>
+    <provides>
+      <version>21</version>
+    </provides>
+    <configuration>
+      <jdkHome>\${env.JAVA_HOME_21_ARM64</jdkHome>
+    </configuration>
+  </toolchain>
+</toolchains>
+`)
+        })
+    })
+})
