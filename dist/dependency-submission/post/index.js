fd87365911aa12c016c307ea21313f351dc53551
bigdaz <bigdaz@users.noreply.github.com>
2024-07-23 02:56:59 +0000

    [bot] Update dist directory

--- dist/dependency-submission/post/index.js
+++ dist/dependency-submission/post/index.js
@@ -99038,7 +99038,7 @@ async function setup(config) {
     maybeExportVariable('DEVELOCITY_AUTO_INJECTION_CUSTOM_VALUE', 'gradle-actions');
     if (config.getBuildScanPublishEnabled()) {
         maybeExportVariable('DEVELOCITY_INJECTION_ENABLED', 'true');
-        maybeExportVariable('DEVELOCITY_PLUGIN_VERSION', '3.17.5');
+        maybeExportVariable('DEVELOCITY_PLUGIN_VERSION', '3.17.6');
         maybeExportVariable('DEVELOCITY_CCUD_PLUGIN_VERSION', '2.0');
         maybeExportVariable('DEVELOCITY_TERMS_OF_USE_URL', config.getBuildScanTermsOfUseUrl());
         maybeExportVariable('DEVELOCITY_TERMS_OF_USE_AGREE', config.getBuildScanTermsOfUseAgree());
