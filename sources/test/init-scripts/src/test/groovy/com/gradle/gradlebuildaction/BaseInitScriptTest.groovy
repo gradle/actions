c3f989640d2af950dbb63a95adaebaefeadd7d82
bot-githubaction <bot-githubaction@gradle.com>
2024-07-23 02:19:07 +0000

    Bump references to Develocity Gradle plugin from 3.17.5 to 3.17.6

--- sources/test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
+++ sources/test/init-scripts/src/test/groovy/com/gradle/gradlebuildaction/BaseInitScriptTest.groovy
@@ -16,7 +16,7 @@ import java.nio.file.Files
 import java.util.zip.GZIPOutputStream
 
 class BaseInitScriptTest extends Specification {
-    static final String DEVELOCITY_PLUGIN_VERSION = '3.17.5'
+    static final String DEVELOCITY_PLUGIN_VERSION = '3.17.6'
     static final String CCUD_PLUGIN_VERSION = '2.0.1'
 
     static final TestGradleVersion GRADLE_3_X = new TestGradleVersion(GradleVersion.version('3.5.1'), 7, 9)
