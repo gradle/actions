7e0f05064581aeb42331f3c4b06109525c87cb58
Daz DeBoer <daz@gradle.com>
2024-08-31 11:11:24 -0600

    Ensure minimum Gradle version for cache-cleanup (#364)
    Instead of always installing and using the latest Gradle version for
    cache cleanup, we now require at least Gradle 8.9.
    
    This avoids downloading and installing Gradle if the version on PATH is
    sufficient to perform cache cleanup.

--- sources/src/execution/gradle.ts
+++ sources/src/execution/gradle.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
+import which from 'which'
+import * as semver from 'semver'
 import * as provisioner from './provision'
 import * as gradlew from './gradlew'
 
@@ -30,4 +32,43 @@ async function executeGradleBuild(executable: string | undefined, root: string, 
     if (status !== 0) {
         core.setFailed(`Gradle build failed: see console output for details`)
     }
+}
+
+export function versionIsAtLeast(actualVersion: string, requiredVersion: string): boolean {
+    const splitVersion = actualVersion.split('-')
+    const coreVersion = splitVersion[0]
+    const prerelease = splitVersion.length > 1
+
+    const actualSemver = semver.coerce(coreVersion)!
+    const comparisonSemver = semver.coerce(requiredVersion)!
+
+    if (prerelease) {
+        return semver.gt(actualSemver, comparisonSemver)
+    } else {
+        return semver.gte(actualSemver, comparisonSemver)
+    }
+}
+
+export async function findGradleVersionOnPath(): Promise<GradleExecutable | undefined> {
+    const gradleExecutable = await which('gradle', {nothrow: true})
+    if (gradleExecutable) {
+        const output = await exec.getExecOutput(gradleExecutable, ['-v'], {silent: true})
+        const version = parseGradleVersionFromOutput(output.stdout)
+        return version ? new GradleExecutable(version, gradleExecutable) : undefined
+    }
+
+    return undefined
+}
+
+export function parseGradleVersionFromOutput(output: string): string | undefined {
+    const regex = /Gradle (\d+\.\d+(\.\d+)?(-.*)?)/
+    const versionString = output.match(regex)?.[1]
+    return versionString
+}
+
+class GradleExecutable {
+    constructor(
+        readonly version: string,
+        readonly executable: string
+    ) {}
 }
