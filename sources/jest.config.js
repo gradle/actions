7063bbed111d20d05e3d4c0dd4853f7a4434eee9
Daz DeBoer <daz@gradle.com>
2026-02-10 12:52:01 -0700

    Switch to esbuild for bundling
    - Minify outputs
    - Emit ESM code

--- sources/jest.config.js
+++ sources/jest.config.js
@@ -1,16 +1,20 @@
-const develocityReporter = require.resolve('@gradle-tech/develocity-agent/jest-reporter');
+import {createRequire} from 'node:module'
 
-module.exports = {
+const require = createRequire(import.meta.url)
+const develocityReporter = require.resolve('@gradle-tech/develocity-agent/jest-reporter')
+
+export default {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts', 'json'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': ['ts-jest', { useESM: true }]
   },
   reporters: [
-      'default',
-      develocityReporter
+    'default',
+    develocityReporter
   ],
   verbose: true
 }
